88ba59f3b0ff0b49d7b5d0fed6999fc101302d79
Marco Dugatto <dughi.boogie@gmail.com>
2024-02-21 18:57:07 +0100

    Fixed Elevators - THANK THA LORD
    It's been a bit chaotic but I ended up improving the workflow for new elevators.
    Now it's not using an AnimationPlayer anymore, instead it uses Tweens to animate the platform at runtime. This way, it's possible to modify every instance of Elevator's animations just by modifying the StartingPosition and EndingPosition nodes.
    Cool beans.

--- GodotProject/Scripts/ActivatableMovableElement.gd
+++ GodotProject/Scripts/ActivatableMovableElement.gd
@@ -2,12 +2,26 @@ class_name ActivatableMovableElement
 extends MovableElement
 
 
-@onready var base_button = $BaseButton
+@export var base_button: Node3D
+@export var animation_duration: float = 2.0
+@onready var starting_position = $StartingPosition
+@onready var ending_position = $EndingPosition
 
 
 func move_platform():
-	super.move_platform()
+	if !activated:
+		var forward_tween = create_tween()
+		forward_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+		forward_tween.set_trans(Tween.TRANS_CUBIC)
+		forward_tween.tween_property($ElevatorPivot, "position:y", ending_position.position.y, animation_duration)
+		activated = true
+	elif two_way_movement:
+		var backward_tween = create_tween()
+		backward_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+		backward_tween.set_trans(Tween.TRANS_CUBIC)
+		backward_tween.tween_property($ElevatorPivot, "position:y", starting_position.position.y, animation_duration)
+		activated = false
 	
-	if !base_button.visible:
+	if base_button!= null && !base_button.visible:
 		base_button.show()
 	

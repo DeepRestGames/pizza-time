66a4e8d898ec8fd361af4e8350d5afefe9421957
Marco Dugatto <dughi.boogie@gmail.com>
2024-02-22 17:39:01 +0100

    Changed all animations to Tweens
    Pushed also old prototype scenes to not have them in the git changes anymore. 
    From this commit onwards, they will most likely be completely broken. To test them out checkout older commits.

--- GodotProject/Scripts/ActivatableMovableElement.gd
+++ GodotProject/Scripts/ActivatableMovableElement.gd
@@ -3,24 +3,10 @@ extends MovableElement
 
 
 @export var base_button: Node3D
-@export var animation_duration: float = 2.0
-@onready var starting_position = $StartingPosition
-@onready var ending_position = $EndingPosition
 
 
 func move_platform():
-	if !activated:
-		var forward_tween = create_tween()
-		forward_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-		forward_tween.set_trans(Tween.TRANS_CUBIC)
-		forward_tween.tween_property($ElevatorPivot, "position:y", ending_position.position.y, animation_duration)
-		activated = true
-	elif two_way_movement:
-		var backward_tween = create_tween()
-		backward_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-		backward_tween.set_trans(Tween.TRANS_CUBIC)
-		backward_tween.tween_property($ElevatorPivot, "position:y", starting_position.position.y, animation_duration)
-		activated = false
+	super.move_platform()
 	
 	if base_button!= null && !base_button.visible:
 		base_button.show()
--- GodotProject/Scripts/MovableElement.gd
+++ GodotProject/Scripts/MovableElement.gd
@@ -2,15 +2,48 @@ class_name MovableElement
 extends Node3D
 
 
-@onready var animation_player = $AnimatableBody3D/AnimationPlayer
+@onready var starting_node = $StartingNode
+@onready var ending_node = $EndingNode
+@onready var animation_pivot = $Pivot
+
 @export var two_way_movement = false
 @export var activated = false
+
+@export_group("Animation variables")
+@export var animation_duration: float = 2.0
+@export var animation_transition_type: Tween.TransitionType = Tween.TRANS_LINEAR
+@export var animation_ease_type: Tween.EaseType = Tween.EASE_IN_OUT
+
+
+# Reset the platform position and rotation on start
+func _ready():
+	pass
+	#if activated:
+		#animation_pivot.position = ending_node.position
+		#animation_pivot.rotation = ending_node.rotation
+	#else:
+		#animation_pivot.position = starting_node.position
+		#animation_pivot.rotation = starting_node.rotation
 
 
 func move_platform():
 	if !activated:
-		animation_player.play("move_forward")
+		_start_animation(true)
 		activated = true
 	elif two_way_movement:
-		animation_player.play("move_backward")
+		_start_animation(false)
 		activated = false
+
+
+func _start_animation(forward: bool):
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.set_trans(animation_transition_type)
+	tween.set_ease(animation_ease_type)
+	
+	if forward:
+		tween.tween_property(animation_pivot, "position", ending_node.position, animation_duration)
+		tween.tween_property(animation_pivot, "rotation", ending_node.rotation, animation_duration)
+	else:
+		tween.tween_property(animation_pivot, "position", starting_node.position, animation_duration)
+		tween.tween_property(animation_pivot, "rotation", starting_node.rotation, animation_duration)

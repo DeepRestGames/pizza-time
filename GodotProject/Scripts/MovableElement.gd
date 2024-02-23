ac2f5de2d642b1d581bafe649149ef2afdc242b5
Marco Dugatto <dughi.boogie@gmail.com>
2024-02-23 13:43:14 +0100

    Movable Elements can now be paused mid-animation
    Just set the flag "is_pausable" to true in the editor

--- GodotProject/Scripts/MovableElement.gd
+++ GodotProject/Scripts/MovableElement.gd
@@ -8,8 +8,9 @@ extends Node3D
 
 @export var two_way_movement = false
 @export var activated = false
+@export var is_pausable = false
 
-@export_group("Animation variables")
+var tween
 @export var animation_duration: float = 2.0
 @export var animation_transition_type: Tween.TransitionType = Tween.TRANS_LINEAR
 @export var animation_ease_type: Tween.EaseType = Tween.EASE_IN_OUT
@@ -17,30 +18,37 @@ extends Node3D
 
 # Reset the platform position and rotation on start
 func _ready():
-	pass
-	#if activated:
-		#animation_pivot.position = ending_node.position
-		#animation_pivot.rotation = ending_node.rotation
-	#else:
-		#animation_pivot.position = starting_node.position
-		#animation_pivot.rotation = starting_node.rotation
+	
+	
+	if activated:
+		animation_pivot.position = ending_node.position
+		animation_pivot.rotation = ending_node.rotation
+	else:
+		animation_pivot.position = starting_node.position
+		animation_pivot.rotation = starting_node.rotation
 
 
 func move_platform():
-	if !activated:
-		_start_animation(true)
-		activated = true
-	elif two_way_movement:
-		_start_animation(false)
-		activated = false
+	# Tween was already created
+	if tween != null and tween.is_valid():
+		# If tween is pausable execute pause logic, otherwise do nothing
+		if is_pausable:
+			if tween.is_running():
+				tween.pause()
+			else:
+				tween.play()
+	# New tween needs to be created
+	else:
+		if !activated:
+			_start_animation(true)
+			activated = true
+		elif two_way_movement:
+			_start_animation(false)
+			activated = false
 
 
 func _start_animation(forward: bool):
-	var tween = create_tween()
-	tween.set_parallel(true)
-	tween.set_trans(animation_transition_type)
-	tween.set_ease(animation_ease_type)
-	
+	tween = create_tween().set_parallel(true).set_trans(animation_transition_type).set_ease(animation_ease_type)
 	if forward:
 		tween.tween_property(animation_pivot, "position", ending_node.position, animation_duration)
 		tween.tween_property(animation_pivot, "rotation", ending_node.rotation, animation_duration)

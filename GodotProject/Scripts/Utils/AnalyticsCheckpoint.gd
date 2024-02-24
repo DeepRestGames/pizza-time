1a179a51594fdb8870490656ae79a96f06851b7a
Marco Dugatto <dughi.boogie@gmail.com>
2024-02-24 14:15:42 +0100

    Added checkpoint-based analytics
    e.g. "When player enters level 1 send event to dashboard"

---
+++ GodotProject/Scripts/Utils/AnalyticsCheckpoint.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+
+enum LevelsSignals {
+	GROUND_LEVEL = 0,
+	FIRST_LEVEL = 1,
+	LABYRINT_LEVEL = 2,
+	MEDITATING_SPHERE_LEVEL = 3,
+	ROTATING_PLATFORMS_LEVEL = 4,
+	CATAPULTS_LEVEL = 5
+}
+
+@export var checkpoint_signal: LevelsSignals
+@onready var checkpoint_area = $Area3D
+
+
+func _on_area_3d_body_entered(body):
+	if body is Player:
+		Analytics.add_event("Entered Level", {"Level": LevelsSignals.find_key(checkpoint_signal)})
+		checkpoint_area.set_deferred("monitoring", false)

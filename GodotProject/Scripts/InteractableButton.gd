946c2ba0e28cf5518b53bff93dfd0bc6368731f9
Marco Dugatto <dughi.boogie@gmail.com>
2024-02-29 13:05:14 +0100

    Added SFXs
    Implemented:
    - Button interact
    - Pizza throw
    - Pizza hit

--- GodotProject/Scripts/InteractableButton.gd
+++ GodotProject/Scripts/InteractableButton.gd
@@ -2,9 +2,15 @@ class_name InteractableButton
 extends StaticBody3D
 
 
+@onready var audio_stream_player = $AudioStreamPlayer3D
 @export var movable_platforms: Array[MovableElement]
+@export var button_sounds: Array[AudioStreamWAV]
 
 
 func activate_button():
+	var audio_stream = button_sounds.pick_random()
+	audio_stream_player.stream = audio_stream
+	audio_stream_player.play()
+	
 	for movable_platform in movable_platforms:
 		movable_platform.move_platform()
--- GodotProject/Scripts/Projectile.gd
+++ GodotProject/Scripts/Projectile.gd
@@ -3,10 +3,16 @@ extends Area3D
 
 const SPEED = 20.0
 @onready var despawn_timer = $DespawnTimer
-
+@onready var audio_stream_player = $AudioStreamPlayer3D
+@export var pizza_throw_sounds: Array[AudioStreamWAV]
+@export var pizza_hit_sounds: Array[AudioStreamWAV]
 
 func _ready():
 	despawn_timer.start()
+	
+	var audio_stream = pizza_throw_sounds.pick_random()
+	audio_stream_player.stream = audio_stream
+	audio_stream_player.play()
 
 
 func _process(delta):
@@ -14,6 +20,11 @@ func _process(delta):
 
 
 func _on_body_entered(body):
+	var audio_stream = pizza_hit_sounds.pick_random()
+	audio_stream_player.stream = audio_stream
+	audio_stream_player.volume_db = 0
+	audio_stream_player.play()
+	
 	if body is Enemy:
 		var enemy = body as Enemy
 		enemy.take_hit()
@@ -26,6 +37,7 @@ func _on_body_entered(body):
 		var npc = body as NPC
 		npc.start_dialogue()
 	
+	await audio_stream_player.finished
 	queue_free()
 
 

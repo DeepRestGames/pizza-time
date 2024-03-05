13c30bf8ea085e84f8332ea472d015f078d463fd
Marco Dugatto <dughi.boogie@gmail.com>
2024-03-05 17:29:19 +0100

    Removed elevators, added death screen and auto respawn

--- GodotProject/Scripts/HUD.gd
+++ GodotProject/Scripts/HUD.gd
@@ -2,6 +2,8 @@ extends Control
 
 
 @onready var checkpoint_label = $CheckpointLabel
+@onready var death_screen = $DeathScreen
+@onready var animation_player = $"../AnimationPlayer"
 
 
 func _ready():
@@ -12,3 +14,12 @@ func _on_checkpoint_reached():
 	checkpoint_label.show()
 	await get_tree().create_timer(3).timeout
 	checkpoint_label.hide()
+
+
+func _on_endless_void_body_entered(_body):
+	print("Fade to black animation start")
+	
+	animation_player.play("fade_to_black")
+	await animation_player.animation_finished
+	GameManager.respawn_player()
+	animation_player.play("fade_to_black", -1, -2, true)

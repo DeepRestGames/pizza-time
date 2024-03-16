b97e60c306dfe7efcffc6fd9245c16c7cab1455f
enirfo <enrico.fornasa.it@gmail.com>
2024-03-16 16:12:19 +0100

    Elevator adjustment

--- GodotProject/Scripts/ActivatableMovableElement.gd
+++ GodotProject/Scripts/ActivatableMovableElement.gd
@@ -3,11 +3,12 @@ extends MovableElement
 
 
 @export var base_button: Node3D
-
-
+	
+	
 func move_platform():
 	super.move_platform()
 	
 	if base_button!= null && !base_button.visible:
 		base_button.show()
 	
+
